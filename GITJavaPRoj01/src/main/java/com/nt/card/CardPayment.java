7e12f9a30edc32cec35c2fef011b88d7ce0c7608
nataraz <natarazjavaarena@gmail.com>
2026-06-14 10:33:43 +0530

    changes are done

---
+++ GITJavaPRoj01/src/main/java/com/nt/card/CardPayment.java
@@ -0,0 +1,12 @@
+package com.nt.card;
+
+public class CardPayment {
+	
+	
+	public   String doCardPayment(long cardNo, double amount) {
+		
+		return  amount+" payment is done from cardNo::"+cardNo;
+		
+	}
+
+}
