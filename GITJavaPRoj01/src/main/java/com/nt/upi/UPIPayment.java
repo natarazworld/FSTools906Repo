2f377ca99ed2fb362667d66c4375c850f3b86abf
nataraz <natarazjavaarena@gmail.com>
2026-06-14 10:02:00 +0530

    Branch1  updation

--- GITJavaPRoj01/src/main/java/com/nt/upi/UPIPayment.java
+++ GITJavaPRoj01/src/main/java/com/nt/upi/UPIPayment.java
@@ -3,7 +3,7 @@ package com.nt.upi;
 public class UPIPayment {
 
 	public String  doPayment(long phNo, double amt) {
-		return  amt+" amount is  send  phoneNumber ::"+phNo;
+		return  amt+" amount is  send  phoneNumber ::"+phNo+" from Bharat Pe";
 	}
 }
 
