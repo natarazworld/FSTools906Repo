c2e7094537c0531f918315c18489ae8b2fb82977
nataraz <natarazjavaarena@gmail.com>
2026-07-10 09:43:21 +0530

    initial Commit

---
+++ DockerWebAppProj022/src/main/java/com/nt/servlet/WishServlet.java
@@ -0,0 +1,31 @@
+//WishServlet.java
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/wish")
+public class WishServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		 //get PrintWriter
+		PrintWriter pw=res.getWriter();
+		//set response content type
+		res.setContentType("text/html");
+		//write the wish message
+		pw.println("<h1  style='color:red;text-align:center'> Good Morning </h1>");
+		
+		//add home hyperlink
+		pw.println("<a href='index.html'>home </a>");
+		//close the stream
+		pw.close();
+	}
+
+}
