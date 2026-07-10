39dce8b753ce3131c2d7f31f95cf7656b077f742
nataraz <natarazjavaarena@gmail.com>
2026-07-10 10:16:51 +0530

    initial commit

---
+++ DockerSBMVCProj03/src/main/java/com/nt/servlet/CustomerOperationsController.java
@@ -0,0 +1,14 @@
+package com.nt.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.GetExchange;
+
+@Controller
+public class CustomerOperationsController {
+
+	@GetMapping("/")
+	public  String showHomePage() {
+		return "welcome";
+	}
+}
---
+++ DockerSBMVCProj03/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,5 @@
+<%@ page isELIgnored="false"%>
+
+
+<h1  style="color:red;text-align:center"> Welcome to Spring  Boot MVC Web application </h1>
+
